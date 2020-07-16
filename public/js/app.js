e1d1b6f806d2211e5943f9c2b3f5c9d13a624608
gurratan <gurratan.grewal@gmail.com>
2020-07-16 19:03:11 -0400

    Removed unsued console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('clinet side javascript file is loaded .. !! ')
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
